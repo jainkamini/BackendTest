1457ce981b22f9f334d58f20bc4674e81137d24f
KAMINI <jainkamini08@gmail.com>
2016-03-06 19:27:11 -0600

    add camera and gallary function

--- app/src/main/java/com/example/android/mytestbackennew/MainActivity.java
+++ app/src/main/java/com/example/android/mytestbackennew/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.android.mytestbackennew;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.CursorLoader;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,25 +34,48 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends Activity {
     public final static String APP_PATH_SD_CARD = "/DesiredSubfolderName/";
     public final static String APP_THUMBNAIL_PATH_SD_CARD = "thumbnails";
     public  static String filenamesave =null;
+    private Camera mCamera = null;
+    private cameraview mCameraView = null;
+    String mCurrentPhotoPath;
+    private File photoFile;
+  final  static int TAKE_PIC =1;
+    final static int SELECT_FILE=2;
 
     private String LOG_TAG=MainActivity.class.getSimpleName();
 
+
+
+
     TextView tv;
     ImageView img1;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv=  (TextView)findViewById(R.id.txthello);
         Bitmap img = BitmapFactory.decodeResource(getResources(), R.drawable.ytlogo);
+        b1=(Button)findViewById(R.id.button);
+        img1=(ImageView)findViewById(R.id.imagefile);
 
-  boolean filesavestatus=     saveImageToExternalStorage(img);
+     //   mCameraView = new cameraview(this, mCamera);//create a SurfaceView to show camera data
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               selectImage();
+
+            }
+        });
+ /* boolean filesavestatus=     saveImageToExternalStorage(img);
+
      //  boolean filesavestatus=  getAlbumStorageDir(getApplicationContext(),img);
 
         String what="gooooo";
@@ -52,12 +84,187 @@ public class MainActivity extends Activity {
         who=filenamesave;
     }
                 String[]args={what,who};
-        img1=(ImageView)findViewById(R.id.imagefile);
+
 
        // new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfredhjghgjhgjhgjhgjhgjhgjhgjgj"));
-        new EndpointsAsyncTask().execute(args);
+        new EndpointsAsyncTask().execute(args);*/
 
        // new EndpointsAsyncTask().execute(argc));
+    }
+
+
+
+
+
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library", "Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    Picturetaken();
+                } else if (items[item].equals("Choose from Library")) {
+                    Intent intent = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Select File"),
+                            SELECT_FILE);
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+
+
+
+    public void Picturetaken()
+    {
+
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            photoFile = createImageFile();
+            takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+            startActivityForResult(takePhotoIntent, TAKE_PIC);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*String pictureFileName="photo10";
+        File pictFoldaer=  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File pictureFile=new File(pictFoldaer,pictureFileName);
+        if (!pictureFile.mkdirs())
+        {
+            Toast.makeText(this, "there are problem creating file ",Toast.LENGTH_LONG).show();
+        }
+
+        if (pictureFile.exists())
+        {
+            Uri outputUri;
+            outputUri= Uri.fromFile((pictureFile));
+            Intent intentStartCamere=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (intentStartCamere.resolveActivity(getPackageManager())!=null)
+            {
+                intentStartCamere.putExtra(MediaStore.EXTRA_OUTPUT,outputUri);
+                startActivityForResult(intentStartCamere,TAKE_PIC);
+            }
+            else
+            {
+                Toast.makeText(this, "cantfind camera",Toast.LENGTH_LONG).show();
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "there are problem creating file ",Toast.LENGTH_LONG).show();
+        }*/
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+       super.onActivityResult(requestCode, resultCode, data);
+
+
+
+
+        if (resultCode==RESULT_OK) {
+            if (requestCode == TAKE_PIC) {
+
+
+                // set the dimensions of the image
+                int targetW = 100;
+                int targetH = 100;
+
+                // Get the dimensions of the bitmap
+                BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                bmOptions.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(photoFile.getAbsolutePath(), bmOptions);
+                int photoW = bmOptions.outWidth;
+                int photoH = bmOptions.outHeight;
+
+                // Determine how much to scale down the image
+                int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+                // Decode the image file into a Bitmap sized to fill the View
+                bmOptions.inJustDecodeBounds = false;
+                bmOptions.inSampleSize = scaleFactor;
+                bmOptions.inPurgeable = true;
+
+                // stream = getContentResolver().openInputStream(data.getData());
+                Bitmap bitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath(), bmOptions);
+                img1.setImageBitmap(bitmap);
+                //  Bundle extras = data.getExtras();
+   /* Bitmap thumnil = (Bitmap) data.getExtras().get("data");
+    img1.setImageBitmap(thumnil);*/
+   /* if (data.getData()==null)
+    {
+
+    }
+    if (data!=null && data.hasExtra("data"))
+    {
+        Bitmap thumnil =data.getParcelableExtra("data");
+        img1.setImageBitmap(thumnil);
+        *//*{
+            Bitmap bp = (Bitmap) data.getExtras().get("data");
+            img1.setImageBitmap(bp);
+        }*//*
+    }*/
+
+            } else  if (requestCode == SELECT_FILE) {
+
+            {
+                Uri selectedImageUri = data.getData();
+                String[] projection = { MediaStore.MediaColumns.DATA };
+                CursorLoader cursorLoader = new CursorLoader(this,selectedImageUri, projection, null, null,
+                        null);
+                Cursor cursor =cursorLoader.loadInBackground();
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+                cursor.moveToFirst();
+                String selectedImagePath = cursor.getString(column_index);
+                Bitmap bm;
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(selectedImagePath, options);
+                final int REQUIRED_SIZE = 200;
+                int scale = 1;
+                while (options.outWidth / scale / 2 >= REQUIRED_SIZE
+                        && options.outHeight / scale / 2 >= REQUIRED_SIZE)
+                    scale *= 2;
+                options.inSampleSize = scale;
+                options.inJustDecodeBounds = false;
+                bm = BitmapFactory.decodeFile(selectedImagePath, options);
+                img1.setImageBitmap(bm);
+            }
+
+            }
+
+        }
+
+
     }
 
 
